0654f45805a54937a745acdf51ecb55c22944669
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-04 09:41:15 +0900

    빈도수 세기까지 풀었습니다.

--- part2/02.배열/(lv.2)빈도수-세기.js
+++ part2/02.배열/(lv.2)빈도수-세기.js
@@ -11,7 +11,15 @@
  */
 
 // TODO: 함수를 작성하세요.
-function getFrequency(arr) {}
+function getFrequency(arr) {
+  const frequencyMap = new Map();
+
+  arr.forEach((element) => {
+    frequencyMap.set(element, (frequencyMap.get(element) || 0) + 1);
+  });
+
+  return Object.fromEntries(frequencyMap);
+}
 
 // export 를 수정하지 마세요.
 export { getFrequency };
