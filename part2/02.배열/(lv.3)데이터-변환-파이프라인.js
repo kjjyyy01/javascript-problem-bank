e5f9f0d64ed5cc43889c03ca56d64d347a2b42af
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-07 09:29:23 +0900

    페이지네이션까지 풀었습니다.

--- part2/02.배열/(lv.3)데이터-변환-파이프라인.js
+++ part2/02.배열/(lv.3)데이터-변환-파이프라인.js
@@ -14,7 +14,13 @@
  * @returns {number[]} 변환된 배열
  */
 
-function transformData(arr, threshold) {}
+function transformData(arr, threshold) {
+  return arr
+    .filter((element) => element % 2 === 0)
+    .map((element) => element ** 2)
+    .filter((element) => element <= threshold)
+    .sort((a, b) => b - a);
+}
 
 // export 를 수정하지 마세요.
 export { transformData };
