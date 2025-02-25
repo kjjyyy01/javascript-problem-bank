b087797b78f4beb1acfa631d429851aefcfff895
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-25 09:53:56 +0900

    설정값 보존하기

--- part2/04.클로저/(lv.3)설정값-보존하기.js
+++ part2/04.클로저/(lv.3)설정값-보존하기.js
@@ -15,7 +15,20 @@
  */
 
 // TODO: createConfigManager 함수를 작성하세요.
-function createConfigManager() {}
+function createConfigManager() {
+  // private config 객체
+  const config = {};
+
+  return {
+    setConfig(key, value) {
+      config[key] = value;
+    },
+
+    getConfig(key) {
+      return config[key];
+    },
+  };
+}
 
 // export 를 수정하지 마세요.
 export { createConfigManager };
